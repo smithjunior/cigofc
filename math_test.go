cabb8e92610b8729a5f47356e01cccfb3e30ddea
Smith Júnior <smith.junior@icloud.com>
2023-01-01 13:32:54 -0300

    Fixed the unit test

--- math_test.go
+++ math_test.go
@@ -1,11 +1,31 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 	total := Soma(10, 2)
 
 	if total != 12 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 12)
+	}
+}
+
+func TestMain(t *testing.T) {
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	main()
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	if string(out) != "12\n" {
+		t.Errorf("Expected %s, got %s", "this is value: 12", out)
 	}
 }
